4e902e437ef961b43ad127c6fb47d2e9eaef35f1
unknown <j.fernando.jfb75@gmail.com>
2022-05-19 22:17:22 -0300

    Alteração de objetos

--- app.py
+++ app.py
@@ -1,57 +1,67 @@
 import streamlit as st
-#import matplotlib.pyplot as plt
 import pandas as pd
-#import seaborn as sns
-#import plotly.express as px
 import pickle
 from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier  
 
-st.title('Relatórios Breast Cancer Wisconsin')
-st.write("## Classificação quanto ao Câncer")
+st.title('Breast Cancer Wisconsin')
+st.write("## Verificação se o câncear é maligno ou benigno")
 #st.write("## Breast Cancer Wisconsin")
 
-st.sidebar.write("### Parâmetros")
-perimeter = st.sidebar.slider("Perimeter", 40.0, 190.0, 100.0)
-area= st.sidebar.slider("Area", 184.0, 4254.0, 881.0)
-compactness = st.sidebar.slider("Compactness", 0.01, 1.6, 0.25)
-concavity = st.sidebar.slider("Concavity", 0.0, 1.30, 0.27)
+st.sidebar.write("### Parâmetros 0")
 
-    
-with open("objetos.pkl", "rb") as arquivo:
-  ss, classifier = pickle.load(arquivo)
+radius0 = st.sidebar.slider("Radius", 6.981000, 28.110000, 0.1)
+texture0 = st.sidebar.slider("Texture", 9.710000, 39.28000, 0.1)
+smoothness0 = st.sidebar.slider("Smoothness", 0.052630, 0.16340, 0.1)
+concave_points0 = st.sidebar.slider("Concave points", 0.0, 0.20120, 0.1)
+symmetry0 = st.sidebar.slider("Symmetry", 0.106000, 0.30400, 0.1)
+fractal_dimension0 = st.sidebar.slider("Fractal dimension", 0.049960, 0.09744, 0.1)
 
-  estrutura = {'perimeter': perimeter, 'area': area, 'compactness': compactness, 'concavity': concavity}
-  df = pd.DataFrame(estrutura, index=[0])
+st.sidebar.write("### Parâmetros 1")
+
+radius1 = st.sidebar.slider("Radius", 0.111500, 2.87300, 0.1)
+texture1 = st.sidebar.slider("Texture", 0.360200, 4.88500, 0.1)
+smoothness1 = st.sidebar.slider("Smoothness", 0.001713, 0.03113, 0.1)
+concave_points1 = st.sidebar.slider("Concave points", 0.0, 0.05279, 0.1)
+symmetry1 = st.sidebar.slider("Symmetry", 0.007882, 0.07895, 0.1)
+fractal_dimension1 = st.sidebar.slider("Fractal dimension", 0.000895, 0.02984, 0.1)
+
+st.sidebar.write("### Parâmetros 2")
+
+radius2 = st.sidebar.slider("Radius", 7.930000, 36.04000, 0.1)
+texture2 = st.sidebar.slider("Texture", 12.020000, 49.54000, 0.1)
+smoothness2 = st.sidebar.slider("Smoothness", 0.071170, 0.22260, 0.1)
+concave_points2 = st.sidebar.slider("Concave points", 0.0, 0.29100, 0.1)
+symmetry2 = st.sidebar.slider("Symmetry", 0.156500, 0.66380, 0.1)
+fractal_dimension2 = st.sidebar.slider("Fractal dimension", 0.055040, 0.20750, 0.1)
+
+arquivo = open("objetos.pkl", "rb")
+ss, classifier = pickle.load(arquivo)
+arquivo.close()
+
+estrutura = {'0radius' : radius0, '0texture': texture0, '0smoothness' : smoothness0, '0concave points': concave_points0,
+               '0symmetry' : symmetry0, '0fractal dimension' : fractal_dimension0, '1radius' : radius1, '1texture': texture1,
+               '1smoothness' : smoothness1, '1concave points': concave_points1, '1symmetry' : symmetry1, '1fractal dimension' : fractal_dimension1,
+               '2radius' : radius2, '2texture': texture2, '2smoothness' : smoothness2, '2concave points': concave_points2,
+               '2symmetry' : symmetry2, '2fractal dimension' : fractal_dimension2}
+  
+df = pd.DataFrame(estrutura, index=[0])
  
-  st.write("### Parâmetros de Entrada")
-  st.write(df)
+st.write("### Parâmetros de Entrada")
+st.write(df)
   
+df = ss.transform(df)
+st.write(df)
   
-  #df = ss.transform(df)
-  #st.write(df)
+#predicao = classifier.predict(df)
+#st.write(f"A classe é: **{predicao[0]}**")
   
-  #predicao = classifier.predict(df)
-  #st.write(f"A classe é: **{predicao[0]}**")
+#predicao = classifier.predict_proba(df)
+#predicao = pd.DataFrame(predicao)
+#predicao.rename({
+    #0 : 'Maligno',
+    #1 : 'Benigno'
+#}, axis=1, inplace=True)
   
-  #predicao = classifier.predict_proba(df)
-  #predicao = pd.DataFrame(predicao)
-  #predicao.rename({
-     #'M' : 0,
-     #'B' : 1
-  #}, axis=1, inplace=True)
-  
-  #st.write("Probabilidades")
-  #st.write(predicao)
-    
- # Visualização Gráfica
-st.title('Visualização Gráfica')
-# Grafico de correlção
-plt.subplots(figsize=(5, 5)) 
-sns.heatmap(train.corr(), annot=True, cmap='Blues')
-ax.set_title('Correlação dos dados')
-fig.tight_layout()
-st.pyplot(fig)
-    
-
- 
+#st.write("Probabilidades")
+#st.write(predicao)
